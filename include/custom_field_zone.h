f89b7062f58b435b0aff675877de4b1228010824
Julien Thevenon <julien_thevenon@yahoo.fr>
2014-03-13 17:23:17 +0100

    Make char color and background color configurable in custom_field_zone

--- include/custom_field_zone.h
+++ include/custom_field_zone.h
@@ -34,12 +34,22 @@ namespace synoptic
 			     const uint32_t & p_width,
 			     const uint32_t & p_height);
     inline void set_content(const uint32_t & p_content);
+    inline void set_char_color(const uint32_t & p_r,
+                               const uint32_t & p_g,
+                               const uint32_t & p_b);
+    inline void set_background_color(const uint32_t & p_r,
+                                     const uint32_t & p_g,
+                                     const uint32_t & p_b);
+    inline void set_char_color(const uint32_t & p_color_code);
+    inline void set_background_color(const uint32_t & p_color_code);
   private:
     virtual const uint64_t & get_content_representation(const uint32_t & p_content)=0;
     // virtual method inherited from zone
     inline void internal_paint(void);
     //end of virtual method inherited from zone
     uint32_t m_content;
+    uint32_t m_char_color;
+    uint32_t m_background_color;
   };
 
   //----------------------------------------------------------------------------
@@ -48,28 +58,54 @@ namespace synoptic
 				       const uint32_t & p_width,
 				       const uint32_t & p_height):
     display_zone(p_owner,p_name,p_width,p_height),
-    m_content(0)
+    m_content(0),
+    m_char_color(p_owner.get_color_code(255,255,255)),
+    m_background_color(p_owner.get_color_code(0,0,0))
       {
       }
+
+  //----------------------------------------------------------------------------
+    void custom_field_zone::set_char_color(const uint32_t & p_r,
+                        const uint32_t & p_g,
+                        const uint32_t & p_b)
+    {
+      m_char_color = get_owner().get_color_code(p_r,p_g,p_b);
+    }
+  //----------------------------------------------------------------------------
+    void custom_field_zone::set_background_color(const uint32_t & p_r,
+                              const uint32_t & p_g,
+                              const uint32_t & p_b)
+    {
+      m_background_color = get_owner().get_color_code(p_r,p_g,p_b);
+    }
+
+    //----------------------------------------------------------------------------
+    void custom_field_zone::set_char_color(const uint32_t & p_color_code)
+    {
+      m_char_color = p_color_code;
+    }
+    //----------------------------------------------------------------------------
+    void custom_field_zone::set_background_color(const uint32_t & p_color_code)
+    {
+      m_background_color = p_color_code;
+    }
 
     //----------------------------------------------------------------------------
     void custom_field_zone::internal_paint(void)
     {
       uint64_t l_content_representation = get_content_representation(m_content);
     
-      uint32_t l_char_color = get_owner().get_color_code(255,255,255);
-      uint32_t l_background_color = get_owner().get_color_code(0,0,0);
       for(uint32_t l_y = 0 ; l_y < get_height() ; ++l_y)
         {
           for(uint32_t l_x = 0 ; l_x < get_width() ; ++l_x)
             {
               if(l_content_representation & 0x1)
                 {
-                  set_pixel(get_width() - 1 - l_x,get_height() - 1 - l_y,l_char_color);
+                  set_pixel(get_width() - 1 - l_x,get_height() - 1 - l_y,m_char_color);
                 }
 	      else
 		{
-                  set_pixel(get_width() - 1 - l_x,get_height() - 1 - l_y,l_background_color);
+                  set_pixel(get_width() - 1 - l_x,get_height() - 1 - l_y,m_background_color);
 		}
               l_content_representation = l_content_representation >> 1;
             }
